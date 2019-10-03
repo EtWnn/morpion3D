a177d748565b7ab63bd5dc9d2e03bb2bb3d24ab7
Lucile Saulnier <lucilesaul.com@gmail.com>
2019-10-03 16:52:16 +0200

    ajout fichier Morpion_Regles

---
+++ Morpion_Regles/regleJeu/Plateau.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace regleJeu
+{
+    static class Plateau
+    {
+
+        public static int[,,] generationPlateau(int taillePlateau)
+        {
+            int[,,] plateau = new int[taillePlateau, taillePlateau, taillePlateau];
+            for (int i = 0; i < taillePlateau; i++)
+            {
+                for (int j = 0; j < taillePlateau; j++)
+                {
+                    for (int k = 0; k < taillePlateau; k++)
+                    {
+                        plateau[i, j, k] = (int)Case.Vide;
+                    }
+                }
+            }
+            return plateau;
+        }
+    }
+}
---
+++ Morpion_Regles/regleJeu/match.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace regleJeu
+{
+    // L'etat d'une case du morpion
+    enum Case
+    {
+        Vide = 0,
+        MotifJoueur1 = 1,
+        MotifJoueur2 = 2,
+        SurbrillanceJoueur1 = 3,
+        SurbrillanceJoueur2 = 4
+    };
+
+    enum ModeJeu
+    {
+        Joueur1,
+        Joueur2,
+    }
+
+    class Match
+    {
+     
+        public ModeJeu Mode { get; set; } //Joueur qui peut jouer 
+        public int[,,] MatricePlateau { get; set; } //La matrice du plateau
+        public bool FinJeu { get; set; } = false; //booleen pour declarer la fin de la partie
+
+        private const int TAILLEPLATEAU = 3; // Dimension du plateau
+        private List<Vector3> PositionsJoueesJoueur1 { get; set; } = new List<Vector3>();
+        private List<Vector3> PositionsJoueesJoueur2 { get; set; } = new List<Vector3>();
+
+        public Match()
+        {
+            MatricePlateau = Plateau.generationPlateau(TAILLEPLATEAU);
+            Mode =ModeJeu.Joueur1;
+        }
+        
+        //Methode pour mettre a jour le morpion en fonction de l'action d un joueur
+        public void Jouer(Vector3 positionJouee)
+        {
+            if (MatricePlateau[(int)positionJouee.X, (int)positionJouee.Y, (int)positionJouee.Z] == (int)Case.Vide)
+            {
+                List<Vector3> listePositionsGagnantes = new List<Vector3>();
+                listePositionsGagnantes = CombinaisonGagnante(positionJouee);
+                if (listePositionsGagnantes.Count==0)
+                {
+                    PlacerJeton(positionJouee, false);
+                }
+                else
+                {
+                   foreach (Vector3 position in listePositionsGagnantes)
+                    {
+                        PlacerJeton(position, true);
+                    }
+                }
+                CalculFinJeu();
+            }
+            else
+            {
+                // a definir
+            }
+        }
+        private void CalculFinJeu()
+        {
+            if (PositionsJoueesJoueur1.Count + PositionsJoueesJoueur2.Count == TAILLEPLATEAU * TAILLEPLATEAU)
+            {
+                FinJeu = true;
+            }
+        }
+        private List<Vector3> CombinaisonGagnante( Vector3 nouvellePosition)
+        {
+            bool combinaisonGagnante = false;
+            List<Vector3> positionsJoueesJoueur = new List<Vector3>();
+            List<Vector3> listePositionsGagnantes = new List<Vector3>();
+            if (Mode== ModeJeu.Joueur1)
+            {
+                positionsJoueesJoueur = PositionsJoueesJoueur1;
+            }
+            else if (Mode == ModeJeu.Joueur2)
+            {
+                positionsJoueesJoueur = PositionsJoueesJoueur2;
+            }
+            if (positionsJoueesJoueur.Count != 0)
+            { 
+            for (int i=0; i<positionsJoueesJoueur.Count; i++)
+            {
+                for (int j=i+1; j < positionsJoueesJoueur.Count; j++)
+                {
+                    combinaisonGagnante = Alignement(nouvellePosition, positionsJoueesJoueur[i], positionsJoueesJoueur[j]);
+                    if (combinaisonGagnante)
+                    {
+                        listePositionsGagnantes.Add(nouvellePosition);
+                        listePositionsGagnantes.Add(positionsJoueesJoueur[i]);
+                        listePositionsGagnantes.Add(positionsJoueesJoueur[j]);
+                        FinJeu = true;
+                        break;
+                    }
+                }
+                if (combinaisonGagnante)
+                {
+                    break;
+                }
+            }
+            }
+            return listePositionsGagnantes;
+        }
+        private void PlacerJeton(Vector3 position, bool combinaisonGagnante)
+        {
+            if (Mode == ModeJeu.Joueur1)
+            {
+                if (combinaisonGagnante)
+                {
+                    MatricePlateau[(int)position.X, (int)position.Y, (int)position.Z] = (int)Case.SurbrillanceJoueur1;
+                }
+                else
+                {
+                    MatricePlateau[(int)position.X, (int)position.Y, (int)position.Z] = (int)Case.MotifJoueur1;
+                    Mode = ModeJeu.Joueur2;
+                }
+                PositionsJoueesJoueur1.Add(position);
+            }
+            else if (Mode == ModeJeu.Joueur2)
+            {
+                if (combinaisonGagnante)
+                {
+                    MatricePlateau[(int)position.X, (int)position.Y, (int)position.Z] = (int)Case.SurbrillanceJoueur2;
+                }
+                else
+                {
+                    MatricePlateau[(int)position.X, (int)position.Y, (int)position.Z] = (int)Case.MotifJoueur2;
+                    Mode = ModeJeu.Joueur1;
+                }
+                PositionsJoueesJoueur2.Add(position);
+            }
+        }
+        private Boolean Alignement(Vector3 point1, Vector3 point2, Vector3 point3)
+        {
+            Boolean aGagne = false;
+            Vector3 vector1 = point1 - point2;
+            Vector3 vector2 = point1 - point3;
+            Vector3 produitVectoriel = Vector3.Cross(vector1, vector2);
+            Vector3 vecteurNul = new Vector3(0, 0, 0);
+            if (produitVectoriel == vecteurNul )
+            {
+                aGagne = true;
+            }
+            return aGagne;
+        }
+
+        public void afficher(int[,,] tab)
+        {
+            string aff = "";
+            for (int x = 0; x < TAILLEPLATEAU; x++)
+            {
+                for (int y = 0; y < TAILLEPLATEAU; y++)
+                {
+                    for (int z = 0; z < TAILLEPLATEAU; z++)
+                    {
+                        aff += tab[x, y, z].ToString() + '\t';
+                    }
+                    aff += '\n';
+                }
+                aff += "\n\n";
+            }
+            Console.WriteLine(aff);
+        }
+
+    }
+}
